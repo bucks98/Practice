4702557a36c4cb91b06ac654fcd89d6fe978d5c9
Bagavath <bagavathprofile@gmail.com>
2024-01-04 14:11:46 +0530

    edited

--- simple.py
+++ simple.py
@@ -5,3 +5,8 @@ my_purchase = {
 grocery_bill = sum(prices[fruit] * my_purchase[fruit]
                    for fruit in my_purchase)
 print ('I owe the grocer $%.2f' % grocery_bill)
+
+
+friends = ['john', 'pat', 'gary', 'michael']
+for i, name in enumerate(friends):
+    print ("iteration {iteration} is {name}".format(iteration=i, name=name))
